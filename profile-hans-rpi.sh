3b498165144700b1226cdc107423a446561eaf46
hanshagen <j.hagen@xs4all.nl>
2026-08-23 21:58:43 +0200

    whatever

---
+++ profile-hans-rpi.sh
@@ -0,0 +1,13 @@
+# This setup is a generic one used on my linux rpi boxes.
+
+export LMT_SETUP_CMD=
+
+export CONTEXTROOT=/data/context
+
+export          LMT_CONTEXT_BIN=$CONTEXTROOT/tex/texmf-linux-aarch64/bin
+export    LMT_MANUAL_LUAMETATEX=$CONTEXTROOT/tex/texmf-context/doc/context/sources/general/manuals/luametatex/luametatex.tex
+export    LMT_MANUAL_LUAMETAFUN=$CONTEXTROOT/tex/texmf-context/doc/context/sources/general/manuals/luametafun/luametafun.tex
+export LMT_MANUAL_MATHINCONTEXT=$CONTEXTROOT/tex/texmf-context/doc/context/sources/general/manuals/mathincontext/mathincontext.tex
+export  LMT_PROFILE_RUNTIME_DIR=
+
+printenv | grep LMT
